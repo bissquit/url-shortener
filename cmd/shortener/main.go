75b1994b0e5c7008df3dae40ff2318d76aae50d5
Egor Vasilyev <e.s.vasilyev@mail.ru>
2025-11-24 23:17:25 +0300

    add url storage

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"sync"
 )
 
 func main() {
@@ -19,7 +22,7 @@ func badRequest(w http.ResponseWriter, message string) {
 	http.Error(w, "Bad request", http.StatusBadRequest)
 }
 
-func shortenURLCreate(w http.ResponseWriter, r *http.Request) {
+func shortenURLCreate(w http.ResponseWriter, r *http.Request, storage *URLStorage) {
 	if r.Header.Get("Content-Type") != "text/plain" {
 		badRequest(w, "Content-Type must be text/plain")
 		return
@@ -38,39 +41,76 @@ func shortenURLCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO: create handler
-	shortURL := "http://localhost:8080/EwHXdJfB"
+	shortenID := generateShortID()
+	storage.Set(shortenID, originalURL)
+	shortURL := "http://" + r.Host + "/" + shortenID
 
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprint(w, shortURL)
 }
 
-func shortenURLRedirect(w http.ResponseWriter, r *http.Request) {
+func shortenURLRedirect(w http.ResponseWriter, r *http.Request, storage *URLStorage) {
 	id := r.URL.Path[1:]
 	if id == "" {
 		badRequest(w, "Invalid Path")
 		return
 	}
 
-	// TODO: create handler
-	originalURL := "https://practicum.yandex.ru/"
+	originalURL, exists := storage.Get(id)
+	if !exists {
+		badRequest(w, "URL not found")
+	}
 
 	w.Header().Set("Location", originalURL)
 	w.WriteHeader(http.StatusTemporaryRedirect)
 }
 
 func run() error {
+	storage := NewURLStorage()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		switch {
 		case r.URL.Path == "/" && r.Method == http.MethodPost:
-			shortenURLCreate(w, r)
+			shortenURLCreate(w, r, storage)
 		case r.URL.Path != "/" && r.Method == http.MethodGet:
-			shortenURLRedirect(w, r)
+			shortenURLRedirect(w, r, storage)
 		default:
 			badRequest(w, fmt.Sprintf("path %s, method %s", r.URL.Path, r.Method))
 		}
 	})
 	return http.ListenAndServe(`:8080`, mux)
+}
+
+// in-memory url storage
+type URLStorage struct {
+	mux  sync.RWMutex
+	data map[string]string
+}
+
+func NewURLStorage() *URLStorage {
+	return &URLStorage{
+		data: make(map[string]string),
+	}
+}
+
+func (s *URLStorage) Set(id, originalURL string) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	s.data[id] = originalURL
+}
+
+func (s *URLStorage) Get(id string) (string, bool) {
+	s.mux.RLock()
+	defer s.mux.RUnlock()
+	url, ok := s.data[id]
+	return url, ok
+}
+
+// id generator
+func generateShortID() string {
+	bytes := make([]byte, 6)
+	rand.Read(bytes)
+	return hex.EncodeToString(bytes)
 }
