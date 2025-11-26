14c8873e126b217b79393aef74c936f54042ffd9
Egor Vasilyev <e.s.vasilyev@mail.ru>
2025-11-26 14:52:51 +0300

    move handlers to separate package

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"log"
 	"net/http"
-	"net/url"
 
-	"github.com/bissquit/url-shortener/internal/repository"
+	"github.com/bissquit/url-shortener/internal/handler"
 	"github.com/bissquit/url-shortener/internal/repository/memory"
-	"github.com/bissquit/url-shortener/internal/service"
 )
 
 func main() {
@@ -18,71 +14,19 @@ func main() {
 	}
 }
 
-func badRequest(w http.ResponseWriter, message string) {
-	log.Printf("bad request: %s", message)
-	http.Error(w, "Bad request", http.StatusBadRequest)
-}
-
-func shortenURLCreate(w http.ResponseWriter, r *http.Request, storage repository.URLRepository) {
-	if r.Header.Get("Content-Type") != "text/plain" {
-		badRequest(w, "Content-Type must be text/plain")
-		return
-	}
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		badRequest(w, "Cannot read request body")
-		return
-	}
-	defer r.Body.Close()
-
-	originalURL := string(body)
-	if _, err := url.ParseRequestURI(originalURL); err != nil {
-		badRequest(w, "Invalid URL")
-		return
-	}
-
-	shortenID, err := service.GenerateUniqID(storage)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	storage.Set(shortenID, originalURL)
-	shortURL := "http://" + r.Host + "/" + shortenID
-
-	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprint(w, shortURL)
-}
-
-func shortenURLRedirect(w http.ResponseWriter, r *http.Request, storage repository.URLRepository) {
-	id := r.URL.Path[1:]
-	if id == "" {
-		badRequest(w, "Invalid Path")
-		return
-	}
-
-	originalURL, exists := storage.Get(id)
-	if !exists {
-		http.Error(w, "URL not found", http.StatusNotFound)
-		return
-	}
-
-	w.Header().Set("Location", originalURL)
-	w.WriteHeader(http.StatusTemporaryRedirect)
-}
-
 func run() error {
 	storage := memory.NewURLStorage()
+	handlers := handler.NewURLHandlers(storage, "http://localhost:8080")
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		switch {
 		case r.URL.Path == "/" && r.Method == http.MethodPost:
-			shortenURLCreate(w, r, storage)
+			handlers.Create(w, r)
 		case r.URL.Path != "/" && r.Method == http.MethodGet:
-			shortenURLRedirect(w, r, storage)
+			handlers.Redirect(w, r)
 		default:
-			badRequest(w, fmt.Sprintf("path %s, method %s", r.URL.Path, r.Method))
+			handler.BadRequest(w, fmt.Sprintf("path %s, method %s", r.URL.Path, r.Method))
 		}
 	})
 	return http.ListenAndServe(`:8080`, mux)
---
+++ internal/handler/url.go
@@ -0,0 +1,78 @@
+package handler
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/bissquit/url-shortener/internal/repository"
+	"github.com/bissquit/url-shortener/internal/service"
+)
+
+type URLHandlers struct {
+	storage repository.URLRepository
+	baseURL string
+}
+
+func NewURLHandlers(storage repository.URLRepository, baseURL string) *URLHandlers {
+	return &URLHandlers{
+		storage: storage,
+		baseURL: baseURL,
+	}
+}
+
+func (h *URLHandlers) Create(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Content-Type") != "text/plain" {
+		BadRequest(w, "Content-Type must be text/plain")
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		BadRequest(w, "Cannot read request body")
+		return
+	}
+	defer r.Body.Close()
+
+	originalURL := string(body)
+	if _, err := url.ParseRequestURI(originalURL); err != nil {
+		BadRequest(w, "Invalid URL")
+		return
+	}
+
+	shortenID, err := service.GenerateUniqID(h.storage)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	h.storage.Set(shortenID, originalURL)
+
+	shortURL := h.baseURL + "/" + shortenID
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprint(w, shortURL)
+}
+
+func (h *URLHandlers) Redirect(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[1:]
+	if id == "" {
+		BadRequest(w, "Invalid Path")
+		return
+	}
+
+	originalURL, exists := h.storage.Get(id)
+	if !exists {
+		http.Error(w, "URL not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Location", originalURL)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func BadRequest(w http.ResponseWriter, message string) {
+	log.Printf("bad request: %s", message)
+	http.Error(w, "Bad request", http.StatusBadRequest)
+}
