6d5668000fbcd96e04ed07c456d0a9ffe434ec6d
Egor Vasilyev <e.s.vasilyev@mail.ru>
2026-04-04 19:11:39 +0300

    fix: run gofmt, fix issues

--- cmd/linter/analyzer/testdata/src/otherpkg/other.go
+++ cmd/linter/analyzer/testdata/src/otherpkg/other.go
@@ -6,7 +6,7 @@ import (
 )
 
 func DoSomething() {
-	log.Fatal("bye")  // want "usage of log.Fatal is not allowed"
-	os.Exit(1)        // want "usage of os.Exit is not allowed"
-	panic("oops")     // want "usage of panic is not allowed"
+	log.Fatal("bye") // want "usage of log.Fatal is not allowed"
+	os.Exit(1)       // want "usage of os.Exit is not allowed"
+	panic("oops")    // want "usage of panic is not allowed"
 }
